3bf633e4fbfcd6104cda56ec9611a6b34e2e3664
MightTechNinja <brianwhitedev1996@outlook.com>
2024-01-25 21:51:38 +0100

    server and scripts initial setup for hosting

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+    const clientBuildPath = path.join(__dirname, "../client/build");
+
+    app.use(
+        express.static(clientBuildPath, { maxAge: 30 * 24 * 60 * 60 * 1000 })
+    );
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(clientBuildPath, "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
