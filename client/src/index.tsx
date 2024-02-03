79c38b45747ddf5d947867ebf5bff5aa5ee873b3
MightTechNinja <brianwhitedev1996@outlook.com>
2024-02-03 13:03:19 +0100

    update imports & file structure improvement

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import "./index.css";
+import "./styles/index.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/header/Header";
-import Sidebar from "../components/sidebar/Sidebar";
+import Header from "@components/header/Header";
+import Sidebar from "@components/sidebar/Sidebar";
 
 const DashboardPage = () => {
     return (
