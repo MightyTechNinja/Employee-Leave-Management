30a6f2618e7f43f75b028b1866292f0713c7de90
MightTechNinja <brianwhitedev1996@outlook.com>
2024-02-11 13:48:03 +0100

    redux store init

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,17 @@
 import "./styles/index.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import sidebarReducer from "./sidebar/sidebarSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        sidebar: sidebarReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
