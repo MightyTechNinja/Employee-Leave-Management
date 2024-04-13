7d380f1a5c0824bb51befd8b9b6ab2be0ccbe471
MightTechNinja <brianwhitedev1996@outlook.com>
2024-04-13 11:39:42 +0200

    fix typo in login page

--- client/src/forms/LoginForm.tsx
+++ client/src/forms/LoginForm.tsx
@@ -72,7 +72,7 @@ const LoginWindow = () => {
                                     to="/password/reset"
                                     className="w-full text-end text-xs text-gray-500 hover:text-gray-700"
                                 >
-                                    Forget your password?
+                                    Forgot your password?
                                 </Link>
                                 <FormControl variant="outlined" fullWidth>
                                     <InputLabel htmlFor="outlined-adornment-password">
